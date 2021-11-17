955bba2cc35e6109bdd629e48271942a1e0ceb28
slahel <74063357+slahel@users.noreply.github.com>
2021-11-17 23:00:28 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -74,8 +74,7 @@ function getForecastDays(coords) {
 
 function showCityWeather(response) {
   console.log(response.data);
-  console.log(Math.round(
-    response.data.main);
+
   celsiusTemp = Math.round(response.data.main.temp);
 
   document
